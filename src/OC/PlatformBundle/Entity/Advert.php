5fc355828076b3f762b8588ae5e911fa33364d10
jojo <faveurextra@gmail.com>
2018-12-22 01:43:48 +0100

    commit3

--- src/OC/PlatformBundle/Entity/Advert.php
+++ src/OC/PlatformBundle/Entity/Advert.php
@@ -1,45 +1,159 @@
 <?php
-// src/OC/PlatformBundle/Entity/Advert.php
 
 namespace OC\PlatformBundle\Entity;
 
-// On définit le namespace des annotations utilisées par Doctrine2
-// En effet, il existe d'autres annotations, on le verra par la suite,
-// qui utiliseront un autre namespace
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * Advert
+ *
+ * @ORM\Table(name="advert")
+ * @ORM\Entity(repositoryClass="OC\PlatformBundle\Repository\AdvertRepository")
  */
 class Advert
 {
     /**
+     * @var int
+     *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    protected $id;
+    private $id;
 
     /**
-     * @ORM\Column(name="date", type="date")
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
      */
-    protected $date;
+    private $date;
 
     /**
+     * @var string
+     *
      * @ORM\Column(name="title", type="string", length=255)
      */
-    protected $title;
+    private $title;
 
     /**
+     * @var string
+     *
      * @ORM\Column(name="author", type="string", length=255)
      */
-    protected $author;
+    private $author;
 
     /**
+     * @var string
+     *
      * @ORM\Column(name="content", type="text")
      */
-    protected $content;
+    private $content;
 
-    // Les getters
-    // Les setters
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Advert
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Advert
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Advert
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Advert
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
 }
+
